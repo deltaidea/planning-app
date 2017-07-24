a43dad1e21e6f5e33cab44f7b39fd07ebbd36035
Nikita Litvin <n@deltaidea.com>
2017-07-24 22:56:30 +0500

    feat(MeetingList): add markup and styles

--- src/SchedulePage/MeetingPanel/MeetingList/MeetingList.js
+++ src/SchedulePage/MeetingPanel/MeetingList/MeetingList.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
-export default class EditMeeting extends Component {
+import './MeetingList.css';
+
+import iconEdit from './icon-edit.png';
+import iconDelete from './icon-delete.png';
+
+export default class MeetingList extends Component {
   render() {
     return (
-      <div>
-        Meeting list
+      <div className="meeting-list-container">
+        <div className="header">
+          <span className="date">September 22</span>
+          <button className="button-create">Create</button>
+        </div>
+        <div className="meeting-list">
+          <div className="item">
+            <div className="title-row">
+              <span className="title">Scheduling a meeting time shouldn't be hard</span>
+              <img src={iconEdit} className="icon edit"/>
+              <img src={iconDelete} className="icon delete"/>
+            </div>
+            <div className="participant">
+              <span>Stella Adler</span>
+            </div>
+          </div>
+          <div className="item">
+            <div className="title-row">
+              <span className="title">Democratic way to coordinate</span>
+              <img src={iconEdit} className="icon edit"/>
+              <img src={iconDelete} className="icon delete"/>
+            </div>
+            <div className="participant">
+              <span>Eddy Arnold</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/SchedulePage/index.js
+++ src/SchedulePage/index.js
@@ -4,13 +4,19 @@ import { Route, Redirect } from 'react-router';
 import CalendarPanel from './CalendarPanel';
 import MeetingPanel from './MeetingPanel';
 
+import './SchedulePage.css';
+
 export default class SchedulePageContainer extends Component {
   render() {
     return (
-      <div>
-        <CalendarPanel/>
-        <Route exact path="/meetings" render={() => <Redirect to="/meetings/2016-09-22"/>}/>
-        <Route path="/meetings/:date(\d{4}-\d{2}-\d{2})" component={MeetingPanel}/>
+      <div className="schedule-container">
+        <div className="schedule-left">
+          <CalendarPanel/>
+        </div>
+        <div className="schedule-right">
+          <Route exact path="/meetings" render={() => <Redirect to="/meetings/2016-09-22"/>}/>
+          <Route path="/meetings/:date(\d{4}-\d{2}-\d{2})" component={MeetingPanel}/>
+        </div>
       </div>
     );
   }
